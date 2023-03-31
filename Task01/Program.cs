ef81eab67a93bdc4b9f2c119b1875dc716baf839
Dmitrii Mikhalev <mixalev89@mail.ru>
2023-03-31 15:12:24 +0800

    Task01 Corrected added

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -3,30 +3,10 @@
 // 782 -> 8
 // 918 -> 1
 
-
-Main();
-
-void Main()
+Console.WriteLine("Введите трехзначное число");
+int a = 0;
+if (int.TryParse(Console.ReadLine(), out a))
 {
-    bool Work = true;
-    while (Work)
-    {
-        System.Console.WriteLine("Решаем задачу? Введите y/n.");
-        string a = Console.ReadLine();
-        switch (a)
-        {
-            case "y": Osn(); break;
-            case "n": Work = false; break;
-        }
-    }
-}
-
-void Osn()
-{
-    Console.WriteLine("Введите трехзначное число");
-    int.TryParse(Console.ReadLine(), out int a);
-    // Вводим трехзначное число если число будет больше 999 или меньше 100 появится сообщение "Не трехзначное число"
-    // Если тпехзначное то производится действия.
     if (a > 999 || a < 100)
     {
         Console.WriteLine("Не трехзначное число");
@@ -38,5 +18,8 @@ void Osn()
         return d;
     }
     Console.WriteLine(GetSecond(a));
-
+}
+else
+{
+    Console.WriteLine("Это не цифра.");
 }
